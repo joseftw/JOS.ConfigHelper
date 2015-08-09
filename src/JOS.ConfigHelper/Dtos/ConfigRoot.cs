de5880e64350e403ef080d27999b96cc75d97894
Josef Ottosson <ottossonjosef@gmail.com>
2015-08-09 14:23:09 +0200

    Removed unused using statements

--- src/JOS.ConfigHelper/Dtos/ConfigRoot.cs
+++ src/JOS.ConfigHelper/Dtos/ConfigRoot.cs
@@ -7,8 +7,6 @@ namespace JOS.ConfigHelper.Dtos
         public Dictionary<string, string> TargetPaths { get; set; } 
         public Dictionary<string, string> ConfigFileVariables { get; set; }
         public DatabaseSettings DatabaseSettings { get; set; } 
-
-
         public string ProjectRootFolder { get; set; }
         public string BaseConfigFilesLocation { get; set; }
     }
--- src/JOS.ConfigHelper/Dtos/DatabaseSettings.cs
+++ src/JOS.ConfigHelper/Dtos/DatabaseSettings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace JOS.ConfigHelper.Dtos
+﻿namespace JOS.ConfigHelper.Dtos
 {
     public class DatabaseSettings
     {
